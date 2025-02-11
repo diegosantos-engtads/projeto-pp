696b4075113fa5c1a1d3d9c0f3e14cddd29b50b4
diegosantos-engtads <diegosantos.engtads@gmail.com>
2025-02-10 21:58:23 -0300

    Mensagem do commit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,22 @@
 'use client'
 
-import { Title } from './styled';
+import { Header, Main, Footer, Title, CopyRight } from './styled';
 
 const Home = () => {
   return (
-    <Title>HOME</Title>
-  )
+    <>
+      <Header>
+        <Title>Projeto PP</Title>
+      </Header>
+      <Main>
+        <h2>Bem-vindo ao Projeto PP!</h2>
+        <p>Iniciando as boas praticas em um projeto modelo.</p>
+      </Main>
+      <Footer>
+        <CopyRight>Desenvolvido por Diego Santos | Site Projeto PP</CopyRight>
+      </Footer>
+    </>
+  );
 }
 
 export default Home;
--- src/app/home/styled.tsx
+++ src/app/home/styled.tsx
@@ -1,6 +1,48 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  background-color: #3498db;
+  color: #fff;
+  padding: 20px 0;
+  text-align: center;
+`;
 
 export const Title = styled.h1`
-  display:flex;
-  color: #c1c
-`
+  font-size: 2.5rem;
+  margin: 0;
+  font-family: 'Arial', sans-serif;
+`;
+
+export const Main = styled.main`
+  padding: 40px 20px;
+  background-color: #f4f4f4;
+  min-height: 60vh;
+  color: #333;
+  text-align: center;
+
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 1.2rem;
+    max-width: 800px;
+    margin: 0 auto;
+  }
+`;
+
+export const Footer = styled.footer`
+  background-color: #2c3e50;
+  color: #ecf0f1;
+  padding: 20px;
+  text-align: center;
+`;
+
+export const CopyRight = styled.p`
+  font-size: 1rem;
+  margin: 0;
+  color: #ecf0f1;
+  font-family: 'Arial', sans-serif;
+  font-style: italic;
+`;
